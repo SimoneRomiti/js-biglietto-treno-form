12da7daa5d890e2d5b5fcaaa568db06ffdfe2239
Simone Romiti <sr1992ei@gmail.com>
2020-11-13 16:57:46 +0100

    Inizio javascript

--- js/script.js
+++ js/script.js
@@ -1,36 +1,58 @@
-// INPUT KILOMETRI
-var numeroKm = parseInt(prompt("Quanti kilometri dovrai fare?"));
-// CONTROLLO KILOMETRI
-while(numeroKm <= 0 || isNaN(numeroKm)) {
-  numeroKm = parseInt(prompt("Il numero dei Kilometri deve essere un numero maggiore di 0, inserisci il numero corretto dei kilometri"));
+
+var genera = document.getElementById("genera");
+var nome;
+var numeroKm;
+var eta;
+
+genera.addEventListener("click",
+function(){
+
+  nome = document.getElementById("input-name").value;
+  console.log(nome);
+
+  numeroKm = document.getElementById("input-km").value;
+  console.log(numeroKm);
+
+  eta = document.getElementById("age").value;
+  console.log(eta);
+
 }
-console.log(numeroKm);
+);
 
-// INPUT ETA'
-var anni = parseInt(prompt("Quanti anni hai?"));
-// CONTROLLO ETA'
-while(anni <= 0 || isNaN(anni)) {
-  anni = parseInt(prompt("L'età deve essere un numero maggiore di zero, inserisci correttamente la tua età"))
-}
-console.log(anni);
 
-// PREZZO SENZA SCONTO
-var prezzoSenzaSconto = numeroKm * 0.21;
-console.log(prezzoSenzaSconto);
 
-// PREZZO CON SCONTI VARI
-var prezzoFinale;
 
-if(anni < 18) {
-  prezzoFinale = prezzoSenzaSconto - ((prezzoSenzaSconto * 20) / 100);
-} else if(anni > 65) {
-  prezzoFinale = prezzoSenzaSconto - ((prezzoSenzaSconto * 40) / 100);
-} else {
-  prezzoFinale = prezzoSenzaSconto;
-}
-console.log(prezzoFinale);
-
-// STAMPA A VIDEO RISULTATO
-document.getElementById('text').innerHTML = "Il prezzo del tuo biglietto è: ";
-
-document.getElementById('number').innerHTML = prezzoFinale.toFixed(2) + "€";
+// // CONTROLLO KILOMETRI
+// while(numeroKm <= 0 || isNaN(numeroKm)) {
+//   numeroKm = parseInt(prompt("Il numero dei Kilometri deve essere un numero maggiore di 0, inserisci il numero corretto dei kilometri"));
+// }
+// console.log(numeroKm);
+//
+// // INPUT ETA'
+// var anni = parseInt(prompt("Quanti anni hai?"));
+// // CONTROLLO ETA'
+// while(anni <= 0 || isNaN(anni)) {
+//   anni = parseInt(prompt("L'età deve essere un numero maggiore di zero, inserisci correttamente la tua età"))
+// }
+// console.log(anni);
+//
+// // PREZZO SENZA SCONTO
+// var prezzoSenzaSconto = numeroKm * 0.21;
+// console.log(prezzoSenzaSconto);
+//
+// // PREZZO CON SCONTI VARI
+// var prezzoFinale;
+//
+// if(anni < 18) {
+//   prezzoFinale = prezzoSenzaSconto - ((prezzoSenzaSconto * 20) / 100);
+// } else if(anni > 65) {
+//   prezzoFinale = prezzoSenzaSconto - ((prezzoSenzaSconto * 40) / 100);
+// } else {
+//   prezzoFinale = prezzoSenzaSconto;
+// }
+// console.log(prezzoFinale);
+//
+// // STAMPA A VIDEO RISULTATO
+// document.getElementById('text').innerHTML = "Il prezzo del tuo biglietto è: ";
+//
+// document.getElementById('number').innerHTML = prezzoFinale.toFixed(2) + "€";
